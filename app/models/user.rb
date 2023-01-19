f60bf101255852dd7f56fe22925072bbdbfad057
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-18 19:25:37 -0500

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many :posts
-  
-    def recent_posts
-        posts.order(created_at: :desc).limit(3)
-    end
+  has_many :posts
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
