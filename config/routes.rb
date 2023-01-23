20a087a570ee1567237d816e6b9cdcc7d029ae69
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-22 20:17:03 -0500

    Render correct views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  root 'users#index'
   get 'users/:id', to: 'users#show'
   get 'users/:id/posts', to: 'posts#index'
   get 'users/:user_id/posts/:id', to: 'posts#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'users#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
