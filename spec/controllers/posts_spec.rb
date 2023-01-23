4b4f71186e7a4f05a7047103cb2913c58b54db15
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-23 13:10:43 -0500

    Write tests for PostsController

--- spec/controllers/posts_spec.rb
+++ spec/controllers/posts_spec.rb
@@ -1,20 +1,37 @@
 require '../rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-    # Check if it renders the correct template for the user's posts url
+    before :each do
+        user = User.create(name: "John Doe", posts_counter: 0)
+        post = user.posts.create(title: "Post 1", comments_counter: 0, likes_counter: 0)
+        user.save!
+        post.author_id = user.id
+        post.save!
+    end
+    
     describe "GET /users/:id/posts" do
-        it "includes the text 'Posts'" do
-          get user_posts_path(1)
-          expect(response).to include("Posts")
-        end
-      end
-
-        # Check if it renders the correct template for the user's post url
-        describe "GET /users/:id/posts/:id" do
-            it "includes the text 'Specific Post'" do
-              get user_post_path(1, 1)
-              expect(response).to include("Specific Post")
-            end
+        let(:user) { User.create(name: "John Doe", posts_counter: 0) }
+        let(:post) { Post.create(title: "Post 1", comments_counter: 0, likes_counter: 0) }
+        it "has a 200 status code" do
+          get :index, params: { user_id: user.id }
+          expect(response).to have_http_status(200)
           end
-
+          it "renders the right template for Posts" do
+            get :index, params: { user_id: user.id }
+            expect(response.body).to render_template(:index)
+          end
+          end
+  
+    describe "GET /users/:id/posts/:id" do
+        let(:user) { User.create(name: "John Doe", posts_counter: 0) }
+        let(:post) { user.posts.create(title: "Post 1", comments_counter: 0, likes_counter: 0) }
+        it "has a 200 status code" do
+          get :show, params: { user_id: user.id, id: post.id }
+          expect(response).to have_http_status(200)
+        end
+        it "renders the right template for Posts" do
+            get :show, params: { user_id: user.id, id: post.id }
+            expect(response).to render_template(:show)
+        end          
+    end  
 end
