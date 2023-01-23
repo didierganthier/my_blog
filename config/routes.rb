9d7a215a180a354ed2e6490c31703e53e8190026
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-23 10:07:53 -0500

    Use ressources helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/:id', to: 'users#show'
-  get 'users/:id/posts', to: 'posts#index'
-  get 'users/:user_id/posts/:id', to: 'posts#show'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'users#index'
   # Defines the root path route ("/")
