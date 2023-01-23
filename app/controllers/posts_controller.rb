04e8e4fbc58ed946bfbcc3e2aff8402e11a0e944
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-23 14:08:42 -0500

    Fix rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,9 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
-  
+
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
-  
 end
--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -1,33 +1,33 @@
 require '../rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-    before :each do
-        user = User.create(name: "John Doe", posts_counter: 0)
-        post = user.posts.create(title: "Post 1", comments_counter: 0, likes_counter: 0)
-        post.author_id = user.id
-        post.save!
-      end
-  
-    describe "GET /" do
-      it "has a 200 status code" do
-        get :index
-        expect(response).to have_http_status(200)
-      end
-      it "renders the template" do
-        get :index
-        expect(response).to render_template(:index)
-      end
+  before :each do
+    user = User.create(name: 'John Doe', posts_counter: 0)
+    post = user.posts.create(title: 'Post 1', comments_counter: 0, likes_counter: 0)
+    post.author_id = user.id
+    post.save!
+  end
+
+  describe 'GET /' do
+    it 'has a 200 status code' do
+      get :index
+      expect(response).to have_http_status(200)
     end
-  
-    describe "GET /users/:id" do
-        let(:user) { User.create(name: "John Doe", posts_counter: 0) }
-        it "has a 200 status code" do
-          get :show, params: { id: user.id }
-          expect(response).to have_http_status(200)
-        end
-        it "renders the template" do
-            get :show, params: { id: user.id }
-            expect(response).to render_template(:show)
-        end
-      end    
+    it 'renders the template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /users/:id' do
+    let(:user) { User.create(name: 'John Doe', posts_counter: 0) }
+    it 'has a 200 status code' do
+      get :show, params: { id: user.id }
+      expect(response).to have_http_status(200)
+    end
+    it 'renders the template' do
+      get :show, params: { id: user.id }
+      expect(response).to render_template(:show)
+    end
+  end
 end
