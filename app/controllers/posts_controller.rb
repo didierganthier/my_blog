5ecc0c185b69500a5a5fc0825c5f6dc5db49e805
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-22 19:52:20 -0500

    Remove comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,3 @@
-# In the PostsController, create an index action that retrieves all the posts for a specified user and renders the index view.
-# In the PostsController, create a show action that retrieves a specified post and its comments and likes and renders the show view.
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
