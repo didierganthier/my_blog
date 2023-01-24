b7feaf92811d2d151953bffd5e26ffbbd414bd07
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-24 13:06:08 -0500

    Show specific post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,6 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+    @comments = Comment.where(post_id: params[:id]).order(created_at: :desc).limit(5)
   end
 end
