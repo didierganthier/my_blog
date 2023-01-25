28b4396cf7aa0610390123af687b6e74c3a38084
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-25 06:43:12 -0500

    Add create method to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,20 @@ class PostsController < ApplicationController
     @post = @user.posts.find(params[:id])
     @comments = Comment.where(post_id: params[:id]).order(created_at: :desc).limit(5)
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+        flash[:notice] = "Post created successfully"
+        redirect_to user_post_path(current_user, @post)
+    else
+        flash[:alert] = "Post creation failed"
+        render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
