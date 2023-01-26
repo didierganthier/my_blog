df15a1f7ff7486a15f422960d999a2a0933d47cd
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-26 12:14:09 -0500

    Fix rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,21 +15,22 @@ class PostsController < ApplicationController
     @comments = Comment.where(post_id: params[:id]).order(created_at: :desc).limit(5)
   end
 
-  def create    
-    @post = Post.new(post_params)    
-    @post.author = current_user    
-    @post.likes_counter = 0    
-    @post.comments_counter = 0    
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    @post.likes_counter = 0
+    @post.comments_counter = 0
     if @post.save
-      flash[:notice] = "Post created successfully"      
-      redirect_to user_posts_path(current_user)    
+      flash[:notice] = 'Post created successfully'
+      redirect_to user_posts_path(current_user)
     else
-      flash[:alert] = "Post creation failed"      
-      render :new, status: :unprocessable_entity    
-    end  
+      flash[:alert] = 'Post creation failed'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
+
   def post_params
     params.require(:post).permit(:title, :text)
   end
