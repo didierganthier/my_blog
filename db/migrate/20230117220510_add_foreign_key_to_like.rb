4804569bc12703126bae27fb34297cd00b191039
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-31 13:46:37 -0500

    Implement auth

--- db/migrate/20230117220510_add_foreign_key_to_like.rb
+++ db/migrate/20230117220510_add_foreign_key_to_like.rb
@@ -1,6 +1,5 @@
 class AddForeignKeyToLike < ActiveRecord::Migration[7.0]
   def change
-    t.references :author, foreign_key: { to_table: 'users' }
-    t.references :post, foreign_key: true
+    add_reference :likes, :author, foreign_key: { to_table: 'users' }
   end
 end
